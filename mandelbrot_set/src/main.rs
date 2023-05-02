0f6baedf93af449ce7c14fb0061e274e298a0a20
nyankyu <fukujinn@gmail.com>
2023-05-02 23:55:12 +0900

    move the calculation part to a separate file

--- mandelbrot_set/src/main.rs
+++ mandelbrot_set/src/main.rs
@@ -1,14 +1,7 @@
 mod mandelbrot_set;
 
-use std::process::exit;
-
-use mandelbrot_set::MandelbrotSet;
-use nannou::{
-    image::{self, ImageBuffer},
-    prelude::*,
-    wgpu::*,
-};
-use num::{complex::Complex64, Complex};
+use mandelbrot_set::*;
+use nannou::{prelude::*, wgpu::*};
 
 fn main() {
     nannou::app(model).run();
@@ -16,7 +9,7 @@ fn main() {
 
 struct Model {
     texture: Texture,
-    //mandelbrot_set: MandelbrotSet,
+    mandelbrot_set: MandelbrotSet,
 }
 
 fn model(app: &App) -> Model {
@@ -37,29 +30,25 @@ fn model(app: &App) -> Model {
         )
         .build(window.device());
 
-    Model { texture }
+    let target_area = Rect::from_xy_wh_f64(
+        dvec2(0.0, 0.0),
+        dvec2(4.0, 4.0),
+    );
+
+    Model {
+        texture: texture,
+        mandelbrot_set: MandelbrotSet::new(
+            1024.0,
+            1024.0,
+            target_area,
+        ),
+    }
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
     frame.clear(BLACK);
 
-    let win_rec = app.window_rect();
-
-    let image = ImageBuffer::from_fn(
-        win_rec.w() as u32,
-        win_rec.h() as u32,
-        |x, y| {
-            let (x, y) = pixel_to_complex(x, y);
-            let r = escape_time(x, y, 510);
-            image::Rgba([
-                ((r * 10) % 255) as u8,
-                ((r * 30) % 255) as u8,
-                ((r * 80) % 255) as u8,
-                255,
-            ])
-        },
-    );
-
+    let image = model.mandelbrot_set.make_image();
     let flat_samples = image.as_flat_samples();
     model.texture.upload_data(
         app.main_window().device(),
@@ -72,28 +61,4 @@ fn view(app: &App, model: &Model, frame: Frame) {
     draw.texture(&model.texture);
 
     draw.to_frame(app, &frame).unwrap();
-}
-
-fn escape_time(x: f64, y: f64, limit: u32) -> u32 {
-    let mut re2 = x * x;
-    let mut im2 = y * y;
-    let mut re = x;
-    let mut im = y;
-
-    let mut i = 0;
-    while i < limit && re2 + im2 <= 4.0 {
-        im = (re + re) * im + y;
-        re = re2 - im2 + x;
-
-        re2 = re * re;
-        im2 = im * im;
-
-        i += 1;
-    }
-    return i;
-}
-
-fn pixel_to_complex(x: u32, y: u32) -> (f64, f64) {
-    let scale = 4.0 / 1024.0;
-    (-2.0 + x as f64 * scale, 2.0 - y as f64 * scale)
 }
--- mandelbrot_set/src/mandelbrot_set.rs
+++ mandelbrot_set/src/mandelbrot_set.rs
@@ -1,1 +1,80 @@
-pub(crate) struct MandelbrotSet {}
+use nannou::{
+    image::{self, ImageBuffer},
+    prelude::*,
+};
+
+pub(crate) struct MandelbrotSet {
+    window_h: f64,
+    window_w: f64,
+    target_area: Rect<f64>,
+}
+
+impl MandelbrotSet {
+    pub(crate) fn new(
+        h: f64,
+        w: f64,
+        target_area: Rect<f64>,
+    ) -> Self {
+        Self {
+            window_h: h,
+            window_w: w,
+            target_area: target_area,
+        }
+    }
+
+    pub(crate) fn make_image(
+        &self,
+    ) -> ImageBuffer<image::Rgba<u8>, Vec<u8>> {
+        ImageBuffer::from_fn(
+            self.window_w as u32,
+            self.window_h as u32,
+            |x, y| {
+                let (x, y) = self.pixel_to_complex(
+                    self.target_area,
+                    x,
+                    y,
+                );
+                let r = escape_time(x, y, 1000);
+                image::Rgba([
+                    (r % 255) as u8,
+                    (r % 255) as u8,
+                    (r % 255) as u8,
+                    255,
+                ])
+            },
+        )
+    }
+
+    fn pixel_to_complex(
+        &self,
+        target_area: Rect<f64>,
+        x: u32,
+        y: u32,
+    ) -> (f64, f64) {
+        let scale_x = target_area.w() / self.window_w;
+        let scale_y = target_area.h() / self.window_h;
+        (
+            target_area.left() + x as f64 * scale_x,
+            target_area.top() - y as f64 * scale_y,
+        )
+    }
+}
+
+fn escape_time(x: f64, y: f64, limit: u32) -> u32 {
+    let mut re2 = x * x;
+    let mut im2 = y * y;
+    let mut re = x;
+    let mut im = y;
+
+    let mut i = 0;
+    while i < limit && re2 + im2 <= 4.0 {
+        im = (re + re) * im + y;
+        re = re2 - im2 + x;
+
+        re2 = re * re;
+        im2 = im * im;
+
+        i += 1;
+    }
+    return i;
+}
