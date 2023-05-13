8737caa269c19d3e588df7249fcaaac703d53baf
nyankyu <fukujinn@gmail.com>
2023-05-14 00:42:26 +0900

    add coloring module

--- mandelbrot_set/src/mandelbrot_set.rs
+++ mandelbrot_set/src/mandelbrot_set.rs
@@ -1,14 +1,18 @@
+use coloring::*;
 use nannou::{
-    image::{self, ImageBuffer},
+    image::{ImageBuffer, Rgba},
     prelude::*,
 };
 use rayon::prelude::*;
 
+mod coloring;
+
 const ZOOM_RATIO: f64 = 0.9;
-const AUTO_ZOOM_RATIO: f64 = 0.995;
+const AUTO_ZOOM_RATIO: f64 = 0.998;
 const AUTO_ZOOM_TARGET: (f64, f64) =
-    (-0.15773294549873199, 1.0253132341831026);
-//(-0.16656641350852985, 1.0362047515983437);
+    //(-0.15773294549873199, 1.0253132341831026);
+(-0.8606130991081128, -0.2351181260185022);
+const ITERATION_LIMIT: u32 = 2_000;
 
 pub(crate) struct MandelbrotSet {
     window_h: u32,
@@ -18,6 +22,7 @@ pub(crate) struct MandelbrotSet {
     scale: f64,
     auto: bool,
     i: u32,
+    //palette: Vec<Rgba<u8>>,
 }
 
 impl MandelbrotSet {
@@ -30,13 +35,13 @@ impl MandelbrotSet {
             scale: 4.0 / h as f64,
             auto: false,
             i: 0,
+            //palette: make_color_palette(ITERATION_LIMIT),
         }
     }
 
     pub(crate) fn make_image(
         &self,
-    ) -> ImageBuffer<image::Rgba<u8>, Vec<u8>> {
-        let limit: u32 = 10_000;
+    ) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
         let mut iteration_counts =
             vec![
                 0;
@@ -57,24 +62,21 @@ impl MandelbrotSet {
                         pixel_x as u32,
                         pixel_y as u32,
                     );
-                    band[pixel_x] =
-                        escape_time(x, y, limit);
+                    band[pixel_x] = Self::escape_time(
+                        x,
+                        y,
+                        ITERATION_LIMIT,
+                    );
                 }
             },
         );
 
-        /*
-                let mut count_per_limit = vec![0; limit as usize];
-                for &count in &iteration_counts {
-                    if count == limit {
-                        continue;
-                    }
-                    count_per_limit[count as usize] += 1;
-                }
-                for i in 1..limit as usize {
-                    count_per_limit[i] += count_per_limit[i - 1];
-                }
-        */
+/*
+        let heatmap = make_cumulative_heatmap(
+            &iteration_counts,
+            ITERATION_LIMIT,
+        );
+*/
 
         ImageBuffer::from_fn(
             self.window_w,
@@ -82,63 +84,20 @@ impl MandelbrotSet {
             |x, y| {
                 let index =
                     (y * self.window_w + x) as usize;
-                /*
-                let r =
-                    (iteration_counts[index] % 256) as u8;
-                    */
-                /*
-                let r = iteration_counts[index] % 510;
-                let r = if r < 256 {
-                    r as u8
-                } else {
-                    (510 - r) as u8
-                };
-                */
-                /*
-                let r = iteration_counts[index] % 256;
-                let r = if r % 10 == 0 {
-                    255
-                } else {
-                    r as u8
-                };
-                */
-                /*
-                let r = if iteration_counts[index] % 2 == 0 {
-                    50
-                } else {
-                    150
-                };
-                */
-                //let r = (iteration_counts[index] % 4) as u8 * 80;
-                /*
-                let r = if iteration_counts[index] == limit {
-                    0
-                } else {
-                    (self.i + iteration_counts[index] % 256) as u8
-                };
-                */
-                /*
                 let count = iteration_counts[index];
-                let r = if count == limit {
-                    0
+                if count == ITERATION_LIMIT {
+                    Rgba([0, 0, 0, 255])
                 } else {
-                    (count_per_limit[count as usize] as f32
-                        / count_per_limit
-                            [limit as usize - 1]
-                            as f32
-                        * 255.0) as u8
-                };
-                */
-
-                let count = iteration_counts[index];
-                let h = count % 200;
-                let hue = if h < 100 { 0.65 } else { 0.0 };
-                let l = if h < 100 {
-                    h as f32 / 100.0
-                } else {
-                    1.0 - ((h - 100) as f32 / 100.0)
-                };
-                image::Rgba(hsl_to_rgba(hue, 1.0, l))
+                    gray_cyclic(count)
+                    //gray_back_and_froth(count)
+                    //gray_strip(count)
+                    //two_color(count)
+                    //four_color(count)
+                    //gray_cyclic_phase(count, self.i)
+                    //gray_back_and_froth_phase(count, self.i)
+                    //self.palette[count as usize]
+                    //gray_heatmap(&heatmap, count)
+                }
             },
         )
     }
@@ -163,7 +122,44 @@ impl MandelbrotSet {
         self.scale *= zoom_rate;
 
         self.i += 1;
-        //println!("{:1.30}", self.center);
+        println!("{:1.30}", self.center);
+    }
+
+    fn escape_time(x: f64, y: f64, limit: u32) -> u32 {
+        let mut re2 = x * x;
+        let mut im2 = y * y;
+        let mut re = x;
+        let mut im = y;
+
+        let mut old_re = 0.0;
+        let mut old_im = 0.0;
+        let mut period = 0;
+
+        let mut i = 0;
+        while i < limit && re2 + im2 <= 4.0 {
+            im = (re + re) * im + y;
+            re = re2 - im2 + x;
+
+            re2 = re * re;
+            im2 = im * im;
+
+            i += 1;
+
+            if abs(re - old_re) < 1e-6
+                && abs(im - old_im) < 1e-6
+            {
+                i = limit;
+                break;
+            }
+
+            period += 1;
+            if period > 20 {
+                period = 0;
+                old_re = re;
+                old_im = im;
+            }
+        }
+        return i;
     }
 
     fn pixel_to_complex(
@@ -191,72 +187,8 @@ impl MandelbrotSet {
                 * (1.0 - AUTO_ZOOM_RATIO);
         self.base_point = self.center + center_to_base;
         self.scale *= AUTO_ZOOM_RATIO;
+        self.i += 1;
 
         //println!("{:1.30}", self.center);
     }
-}
-
-fn escape_time(x: f64, y: f64, limit: u32) -> u32 {
-    let mut re2 = x * x;
-    let mut im2 = y * y;
-    let mut re = x;
-    let mut im = y;
-
-    let mut old_re = 0.0;
-    let mut old_im = 0.0;
-    let mut period = 0;
-
-    let mut i = 0;
-    while i < limit && re2 + im2 <= 4.0 {
-        im = (re + re) * im + y;
-        re = re2 - im2 + x;
-
-        re2 = re * re;
-        im2 = im * im;
-
-        i += 1;
-
-        if abs(re - old_re) < 1e-6
-            && abs(im - old_im) < 1e-6
-        {
-            i = limit;
-            break;
-        }
-
-        period += 1;
-        if period > 20 {
-            period = 0;
-            old_re = re;
-            old_im = im;
-        }
-    }
-    return i;
-}
-
-fn hsl_to_rgba(h: f32, s: f32, l: f32) -> [u8; 4] {
-    let c = (1.0 - (2.0 * l - 1.0).abs()) * s;
-    let x = c * (1.0 - ((h * 6.0) % 2.0 - 1.0).abs());
-    let m = l - c / 2.0;
-
-    let (r, g, b) = if h < 1.0 / 6.0 {
-        (c, x, 0.0)
-    } else if h < 2.0 / 6.0 {
-        (x, c, 0.0)
-    } else if h < 3.0 / 6.0 {
-        (0.0, c, x)
-    } else if h < 4.0 / 6.0 {
-        (0.0, x, c)
-    } else if h < 5.0 / 6.0 {
-        (x, 0.0, c)
-    } else {
-        (c, 0.0, x)
-    };
-
-    let (r, g, b) = (
-        ((r + m) * 255.0).round() as u8,
-        ((g + m) * 255.0).round() as u8,
-        ((b + m) * 255.0).round() as u8,
-    );
-
-    [r, g, b, 255]
 }
---
+++ mandelbrot_set/src/mandelbrot_set/coloring.rs
@@ -0,0 +1,139 @@
+#![allow(dead_code)]
+
+use nannou::image::Rgba;
+
+pub(super) fn gray_cyclic(count: u32) -> Rgba<u8> {
+    let gray = (count % 256) as u8;
+    Rgba([gray, gray, gray, 255])
+}
+
+pub(super) fn gray_back_and_froth(count: u32) -> Rgba<u8> {
+    let gray = count % 510;
+    let gray = if gray <= 255 {
+        gray as u8
+    } else {
+        (510 - gray) as u8
+    };
+    Rgba([gray, gray, gray, 255])
+}
+
+pub(super) fn gray_strip(count: u32) -> Rgba<u8> {
+    let gray = count % 256;
+    let gray =
+        if gray % 10 == 0 { 255 } else { gray as u8 };
+    Rgba([gray, gray, gray, 255])
+}
+
+pub(super) fn two_color(count: u32) -> Rgba<u8> {
+    if count % 2 == 0 {
+        Rgba([50, 50, 50, 255])
+    } else {
+        Rgba([150, 150, 150, 255])
+    }
+}
+
+pub(super) fn four_color(count: u32) -> Rgba<u8> {
+    let gray = (count % 4) as u8 * 80;
+    Rgba([gray, gray, gray, 255])
+}
+
+pub(super) fn gray_cyclic_phase(
+    count: u32,
+    phase: u32,
+) -> Rgba<u8> {
+    let gray = ((count + phase) % 256) as u8;
+    Rgba([gray, gray, gray, 255])
+}
+
+pub(super) fn gray_back_and_froth_phase(
+    count: u32,
+    phase: u32,
+) -> Rgba<u8> {
+    let gray = (count + phase) % 510;
+    let gray = if gray <= 255 {
+        gray as u8
+    } else {
+        (510 - gray) as u8
+    };
+    Rgba([gray, gray, gray, 255])
+}
+
+pub(super) fn make_color_palette(
+    size: u32,
+) -> Vec<Rgba<u8>> {
+    let mut palette = Vec::<Rgba<u8>>::with_capacity(200);
+
+    for i in 0..size {
+        let h = i % 200;
+        let hue = if h < 100 { 0.65 } else { 0.0 };
+        let l = if h < 100 {
+            h as f32 / 100.0
+        } else {
+            1.0 - ((h - 100) as f32 / 100.0)
+        };
+
+        palette.push(hsl_to_rgba(hue, 1.0, l));
+    }
+
+    palette
+}
+
+pub(super) fn make_cumulative_heatmap(
+    iteration_counts: &Vec<u32>,
+    limit: u32,
+) -> Vec<u32> {
+    let mut heatmap = vec![0; limit as usize];
+
+    for &count in iteration_counts {
+        if count == limit {
+            continue;
+        }
+        heatmap[count as usize] += 1;
+    }
+
+    for i in 1..limit as usize {
+        heatmap[i] += heatmap[i - 1];
+    }
+
+    heatmap
+}
+
+pub(super) fn gray_heatmap(
+    heatmap: &Vec<u32>,
+    count: u32,
+) -> Rgba<u8> {
+    let total = heatmap[heatmap.len()-2];
+    let r = (heatmap[count as usize] as f32
+        / total as f32
+        * 255.0) as u8;
+
+    Rgba([r, r, r, 255])
+}
+
+fn hsl_to_rgba(h: f32, s: f32, l: f32) -> Rgba<u8> {
+    let c = (1.0 - (2.0 * l - 1.0).abs()) * s;
+    let x = c * (1.0 - ((h * 6.0) % 2.0 - 1.0).abs());
+    let m = l - c / 2.0;
+
+    let (r, g, b) = if h < 1.0 / 6.0 {
+        (c, x, 0.0)
+    } else if h < 2.0 / 6.0 {
+        (x, c, 0.0)
+    } else if h < 3.0 / 6.0 {
+        (0.0, c, x)
+    } else if h < 4.0 / 6.0 {
+        (0.0, x, c)
+    } else if h < 5.0 / 6.0 {
+        (x, 0.0, c)
+    } else {
+        (c, 0.0, x)
+    };
+
+    let (r, g, b) = (
+        ((r + m) * 255.0).round() as u8,
+        ((g + m) * 255.0).round() as u8,
+        ((b + m) * 255.0).round() as u8,
+    );
+
+    Rgba([r, g, b, 255])
+}
